868871a552c2323fd2f8567819a0d7ce12067cd8
Andres Henao <henaoandres2405@gmail.com>
2025-04-18 14:44:49 -0500

    inicio capacitor

---
+++ android/app/src/main/java/com/jitcall/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.jitcall.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.jitcall.app',
   appName: 'jitCall',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+    "PushNotifications": {
+        "presentationOptions": ["badge", "sound", "alert"]
+    }
+  },
 };
 
 export default config;
